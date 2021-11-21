7c0e7ba5d28ec26f34a619e69fe695554e04e8d9
npclown <nigrumspiritus@gmail.com>
2021-11-21 16:26:37 +0900

    초기 파일 구성

---
+++ Src/HurryUp_Agent/CMessage.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "stdafx.h"
+
+//TODO :: 메시지 관리 클래스 구현
+class CMessage
+{
+private:
+
+public:
+};
+
---
+++ Src/HurryUp_Agent/main.cpp
@@ -0,0 +1,7 @@
+﻿#include <cstdio>
+
+int main()
+{
+    printf("%s에 오신 것을 환영합니다!\n", "New_HurryUp_Agent");
+    return 0;
+}
