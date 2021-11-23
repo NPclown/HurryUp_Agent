ff6638b9c070af5e8fa15d9597c0877441f34ce6
npclown <nigrumspiritus@gmail.com>
2021-11-23 20:49:53 +0900

    Collector 클래스 구현

--- Src/HurryUp_Agent/CCollector.cpp
+++ Src/HurryUp_Agent/CCollector.cpp
@@ -1,1 +1,33 @@
 #include "CCollector.h"
+
+CCollector::CCollector()
+{
+    this->device = new CDevice();
+    this->monitoring = new CMonitoring();
+}
+
+CCollector::~CCollector()
+{
+}
+
+CCollector* CCollector::GetInstance(void)
+{
+    static CCollector instance;
+    return &instance;
+}
+
+void CCollector::init()
+{
+    std::future<void> deviceInfo = std::async(std::launch::async, &CDevice::collectAllData, this->device);
+    std::future<void> monitoringStart = std::async(std::launch::async, &CMonitoring::StartMonitoring, this->monitoring);
+}
+
+CDevice* CCollector::DeviceInstance(void)
+{
+    return this->device;
+}
+
+CMonitoring* CCollector::MonitoringInstance(void)
+{
+    return this->monitoring;
+}
