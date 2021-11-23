5edf1452e545475f0e801a460721d9d2273a5737
npclown <nigrumspiritus@gmail.com>
2021-11-23 15:00:11 +0900

    통신 클래스 구현

--- Src/HurryUp_Agent/CCommunication.h
+++ Src/HurryUp_Agent/CCommunication.h
@@ -1,19 +1,36 @@
 #pragma once
 #include "stdafx.h"
+#include <unistd.h>>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "CMessage.h"
+
+#define BUFFER_SIZE 4096
 
 //TODO :: TCP 통신 싱글톤 클래스 구현
 class CCommunication
 {
 private:	
-	//TODO :: 소캣 변수
-	//TODO :: 메시지 보관 버퍼
-	//TODO :: 서버 주소
+	int clientSocket;
+	int connected;
+	struct sockaddr_in serverAddress;
+	std::tstring messageBuffers;
 
-	//TODO :: 생존 여부 함수 구현
+	CCommunication();
+
+	bool Live();
 public:
-	//TODO :: 연결 함수 구현
-	//TODO :: 메시지 송신 함수 구현
-	//TODO :: 메시지 수신 함수 구현
-	//TODO :: 연결 해제 함수 구현
+	static CCommunication* GetInstance();
+
+	void Init(std::tstring ip, std::tstring port);
+	void Connect();
+	void Start();
+	void Send();
+	void Recv();
+	void Disconnect();
 };
 
+inline CCommunication* CommunicationManager()
+{
+	return CCommunication::GetInstance();
+}
